649b1daeba9e862ef732d246c7f29f04523ee831
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-07-19 23:45:00 -0300

    feat: classes structure

---
+++ src/org/iPhone/AparelhoTelefonico.java
@@ -0,0 +1,8 @@
+package org.iPhone;
+
+public interface AparelhoTelefonico {
+    public void ligar(String numero);
+    public void atender();
+    public void iniciarCorreioVoz();
+
+}
---
+++ src/org/iPhone/NavegadorInternet.java
@@ -0,0 +1,7 @@
+package org.iPhone;
+
+public interface NavegadorInternet {
+    public void exibirPagina(String url);
+    public void adicionarNovaAba();
+    public void atualizarPagina();
+}
